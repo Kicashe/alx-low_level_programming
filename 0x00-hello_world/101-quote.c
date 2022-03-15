6e300f330eab2d0b7117d2547ca624e147247c58
Kicashe <keithkwanele@gmail.com>
2022-03-15 01:21:04 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - print quote
  *
  * Description: prints Dor's quote
  *
- *Return: 0 (Success)
+ *Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59;
