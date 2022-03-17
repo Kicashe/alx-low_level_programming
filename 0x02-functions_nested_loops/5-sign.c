9fb99d6740662191a63b6a388ee76e701fb10cbe
Kicashe <keithkwanele@gmail.com>
2022-03-17 20:12:54 +0000

    putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *
- *@n: the int to print
+ * print_sign - prints the sign of a number
+ * @n: the int to print
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign (int n)
 {
