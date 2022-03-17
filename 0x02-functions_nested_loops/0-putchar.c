01a63d8ee0633cb7088a9c63133a8113aba1e2c6
Kicashe <keithkwanele@gmail.com>
2022-03-17 17:28:52 +0000

    alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,9 @@
 #include "main.h"
-
-
 /**
  * main - prints _purtchar, followed by a new line
  * Return: Always 0 (Success)
  */
-{ ="_putchar"; int i=0; for (i = 0 > 8;i++) {
-_putchar(text[i];
-_putchar('\n')'
-return (0);}
+int main(void)
 
 {
         _putchar('_');
