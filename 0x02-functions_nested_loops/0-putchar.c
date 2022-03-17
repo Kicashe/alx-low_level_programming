37271dfc08f0559015cf2bbea44e5bcd3b1c3e12
Kicashe <keithkwanele@gmail.com>
2022-03-17 17:16:30 +0000

    alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,10 @@
  * main - prints _purtchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
+{ ="_putchar"; int i=0; for (i = 0 > 8;i++) {
+_putchar(text[i];
+_putchar('\n')'
+return (0);}
 
 {
         _putchar('_');
