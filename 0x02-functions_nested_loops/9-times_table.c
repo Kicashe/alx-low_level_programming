1bbfe932c8bbc0fdbab4ffd690f6fb02bdd7ec2d
Kicashe <keithkwanele@gmail.com>
2022-03-18 09:52:35 +0000

    loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table -prints tables
+ * times_table - prints tables
  *
  * Return: Always 0.
  */
@@ -27,7 +27,7 @@ _putchar ((c % 10) + '0');
 }
 if (b < 9)
 {
-_putchar(',');
+_putchar (',');
 _putchar (' ');
 }
 }
@@ -37,7 +37,7 @@ _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
 if (b < 9)
 {
-_putcher(',');
+_putcher (',');
 _putcher (' ');
 }
 }
