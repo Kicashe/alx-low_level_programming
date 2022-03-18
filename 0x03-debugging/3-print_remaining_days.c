9515289de470f10aa581a8b7022d21735df936dd
Kicashe <keithkwanele@gmail.com>
2022-03-18 03:23:44 +0000

    my commit messge

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,7 +5,7 @@
 * left in the year, taking leap years into account
 * @month: month in number format
 * @day: day of the month
-* year: year
+* @year: year
 * Return: void
 */
 
@@ -32,6 +32,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
 else
 {
 printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
