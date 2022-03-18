b28aa211c91d9249fe17058b60a56b009360255a
Kicashe <keithkwanele@gmail.com>
2022-03-18 05:04:48 +0000

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_most_numbers - print  numbers
+ * print_most_numbers - print numbers
  *
- Return: Always 0.
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
 
-init i;
+int i;
 
 i = 0;
 while (i < 10)
