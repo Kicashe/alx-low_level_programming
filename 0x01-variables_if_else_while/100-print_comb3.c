ad481e5046cf4b71a926dcad84f204dc2cd57157
Kicashe <keithkwanele@gmail.com>
2022-03-21 14:34:18 +0000

    my commit messge

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success/completed)
  */
 int main(void)
-
 {
-int c, i;
+         int c, i;
 
-for (c = '0'; c <= '9'; c++)
-{
-for (i = '0'; i <= '9'; i++)
-{
-if (c < i)
-{
-putchar(c);
-putchar (i);
+         for (c = '0'; c <= '9'; c++)
+         {
+                 for (i = '0'; i <= '9'; i++)
+                 {
+                         if (c < i)
+                         {
+                                 putchar(c);
+                                 putchar (i);
 
+                                 if (c != '8' || (c == '8' && i != '9'))
+                                 {
 
-if (c != '8' || (c == '8' && i != '9'))
-{
-break;
-{
-putchar(',');
-putchar(' ');
-}
+                                        putchar(',');
+                                        putchar(' ');
+                                 }
 
-}
+                         }
 
-}
+                 }
 
-}
+          }
 
-putchar('\n');
+          putchar('\n');
 
-return (0);
-
+          return (0);
 }
