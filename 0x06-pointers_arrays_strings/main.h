341fc75f138525a99903e83baf71fd2471d572a1
Kicashe <keithkwanele@gmail.com>
2022-03-24 17:18:56 +0000

    pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *print_number(int n);
 char *rot13(char *);
-char *infinite_add(char n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
