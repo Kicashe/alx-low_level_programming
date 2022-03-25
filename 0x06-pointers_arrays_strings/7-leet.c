ad98cbe94829c40c3f871bf1e4bbdba1403f9cb2
Kicashe <keithkwanele@gmail.com>
2022-03-25 04:31:22 +0000

    pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,13 +3,13 @@
  * leet - encodes a string into 1337
  * @s: input string.
  * Return: the pointer dest.
-*/
+ */
 
 char *leet(char *s)
 {
-int count = 0; i;
-int lower_case[] = {97, 101, 111,116, 108};
-int upper_case[] = {65, 69 , 79, 84, 76};
+int count = 0, i;
+int lower_case[] = {97, 101, 111, 116, 108};
+int upper_case[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
 
@@ -19,7 +19,7 @@ for (i = 0; i < 5; i++)
 {
 if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 {
-*(s + count) = number[i];
+*(s + count) = numbers[i];
 break;
 }
 }
