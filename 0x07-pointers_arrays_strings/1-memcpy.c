b6c54333263130321a730d88bef36c7a675f8218
Kicashe <keithkwanele@gmail.com>
2022-03-28 16:57:17 +0000

    arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_memcpy -   copy number form adress "from" to adress "to"
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
  *@dest: content is to be copied
  *@src: Source of data to be copied
  *@n: bytes of the memory
