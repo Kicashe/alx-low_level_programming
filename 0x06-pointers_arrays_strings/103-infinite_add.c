9edba44864e0ad1685a12c19ed0b22c862d3ecb1
Kicashe <keithkwanele@gmail.com>
2022-03-28 14:42:16 +0000

    pointers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -39,16 +39,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
                         *(r + bg) = '0';
                 if (c1 > 0)
                         c1--, dr1 = *(n1 + c1) - 48;
-                elsee
+                else
                         dr1 = 0;
                 if (c2 > 0)
                         c2--, dr2 = *(n2 + c2) - 48;
                 else
                         dr2 = 0;
                 bg--, size_r--;
-         }
-         if (*(r) == '0')
+        }
+        if (*(r) == '0')
                 return (r + 1);
-         else
+        else
                 return (r);
 }
